08c81b1f9002576b3c07a2a3eb15c1c3a99278f0
RahulCompani7 <rahulcompani7@gmail.com>
2024-06-09 22:31:36 +0530

    New Changes made

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,218 @@
+"use client";
+
+import React from "react";
+import { FaLinkedin, FaGithub, FaTwitter, FaInstagram } from "react-icons/fa";
+
+import Navbar from "@/components/Nav";
 import Image from "next/image";
+import profile from "../assets/Blob.png";
+import htmlLogo from "../assets/html-5.png";
+import cssLogo from "../assets/css-3.png";
+import javaLogo from "../assets/java.png";
+import javascriptLogo from "../assets/js.png";
+import mysqlLogo from "../assets/mysql.png";
+import nextLogo from "../assets/next.png";
+import nodeLogo from "../assets/node.png";
+import reactLogo from "../assets/physics.png";
+import typescriptLogo from "../assets/typescript.png";
+import uipathLogo from "../assets/Uipath.jpg";
+import mongoDbLogo from "../assets/mongodb-original-wordmark-icon-2015x2048-n6r3kuri.png";
+import { motion } from "framer-motion";
+import { FlipWords } from "../components/ui/flip-words";
+import { CardBody, CardContainer, CardItem } from "../components/ui/3d-card";
+import "./home.css";
+import { AuroraBackground } from "../components/ui/aurora-background";
 
 export default function Home() {
+  const words = ["a Software Developer", "a Tech Enthusiast"];
+
+  const variants = {
+    initial: { opacity: 0, y: 20 },
+    animate: { opacity: 1, y: 0 },
+  };
+
+  const imageVariants = {
+    initial: { opacity: 0, scale: 0.8 },
+    animate: { opacity: 1, scale: 1 },
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <>
+      <Navbar />
+
+      <section className="section">
+        <div className="section__container">
+          <motion.div
+            initial="initial"
+            animate="animate"
+            variants={variants}
+            className="content"
           >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+            <p className="subtitle">HELLO</p>
+            <h1 className="title">
+              I'm{" "}
+              <span>
+                Rahul
+                <br />
+                <FlipWords words={words} />
+              </span>
+            </h1>
+            <p className="description">
+              Welcome to my portfolio! I'm Rahul Compani, a dynamic and driven
+              professional immersed in the ever-evolving world of technology.
+              With a passion for innovation and a knack for problem-solving, I
+              navigate the digital landscape with agility and finesse. As a tech
+              enthusiast and digital craftsman, I thrive on pushing boundaries
+              and exploring new frontiers. Equipped with a blend of creativity
+              and technical prowess, I craft digital experiences that inspire
+              and delight. Join me on this journey of exploration and
+              innovation, as we shape the future of technology together.
+            </p>
+            <div className="social-icons-container">
+              <div className="social-icons">
+                <a
+                  href="https://www.linkedin.com/in/rahul-compani-713a12210"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <FaLinkedin className="icon linkedin" />
+                </a>
+                <a
+                  href="https://github.com/RahulCompani7"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <FaGithub className="icon github" />
+                </a>
+                <a
+                  href="https://x.com/RahulCompani"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <FaTwitter className="icon twitter" />
+                </a>
+                <a
+                  href="https://www.instagram.com/rahulcompani"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <FaInstagram className="icon instagram" />
+                </a>
+              </div>
+            </div>
+          </motion.div>
+          <CardContainer className="inter-var">
+            <CardBody className="bg-gray-50 relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[30rem] h-auto rounded-xl p-6 border  ">
+              <CardItem translateZ="100" className="w-full mt-4">
+                <motion.div
+                  initial="initial"
+                  animate="animate"
+                  variants={imageVariants}
+                  className="Profileimage"
+                >
+                  <Image
+                    src={profile}
+                    height="500"
+                    width="500"
+                    className="h-60 w-full object-cover rounded-xl group-hover/card:shadow-xl"
+                    alt="thumbnail"
+                  />
+                </motion.div>
+              </CardItem>
+            </CardBody>
+          </CardContainer>
         </div>
-      </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+        <div className="section__container1">
+          <motion.div
+            initial="initial"
+            animate="animate"
+            variants={variants}
+            className="content"
+          >
+            {" "}
+            <div className="container">
+              <h1>This is what I am good at</h1>
+              <div className="skills">
+                <div className="skill">
+                  <Image src={htmlLogo} alt="HTML" width={100} height={100} />
+                  <p>HTML</p>
+                </div>
+                <div className="skill">
+                  <Image src={cssLogo} alt="CSS" width={100} height={100} />
+                  <p>CSS</p>
+                </div>
+                <div className="skill">
+                  <Image
+                    src={javascriptLogo}
+                    alt="JavaScript"
+                    width={100}
+                    height={100}
+                  />
+                  <p>JavaScript</p>
+                </div>
+                <div className="skill">
+                  <Image src={javaLogo} alt="Java" width={100} height={100} />
+                  <p>Java</p>
+                </div>
+                <div className="skill">
+                  <Image
+                    src={typescriptLogo}
+                    alt="TypeScript"
+                    width={100}
+                    height={100}
+                  />
+                  <p>TypeScript</p>
+                </div>
+                <div className="skill">
+                  <Image src={reactLogo} alt="React" width={100} height={100} />
+                  <p>React.js</p>
+                </div>
+                <div className="skill">
+                  <Image
+                    src={nextLogo}
+                    alt="Next.js"
+                    width={100}
+                    height={100}
+                  />
+                  <p>Next.js</p>
+                </div>
+                <div className="skill">
+                  <Image
+                    src={nodeLogo}
+                    alt="Node.js"
+                    width={100}
+                    height={100}
+                  />
+                  <p>Node.js</p>
+                </div>
+                <div className="skill">
+                  <Image src={mysqlLogo} alt="MySQL" width={100} height={100} />
+                  <p>MySQL</p>
+                </div>
+                <div className="skill">
+                  <Image
+                    src={mongoDbLogo}
+                    alt="MongoDB"
+                    width={100}
+                    height={100}
+                  />
+                  <p>MongoDB</p>
+                </div>
+                <div className="skill">
+                  <Image
+                    src={uipathLogo}
+                    alt="UiPath"
+                    width={100}
+                    height={100}
+                  />
+                  <p>UiPath</p>
+                </div>
+                {/* Add other skills in a similar manner */}
+              </div>
+            </div>
+          </motion.div>
+        </div>
+      </section>
+    </>
   );
 }
---
+++ src/components/Nav.tsx
@@ -0,0 +1,35 @@
+import "./nav.css";
+import Link from "next/link";
+
+export default function Navbar() {
+  return (
+    <nav>
+      <div className="nav__content">
+        <div className="logo">
+          <Link href="/">Rahul Compani</Link>
+        </div>
+        <label htmlFor="check" className="checkbox">
+          <i className="ri-menu-line"></i>
+        </label>
+        <input type="checkbox" name="check" id="check" />
+        <ul>
+          <li>
+            <Link href="/">Home</Link>
+          </li>
+          <li>
+            <Link href="/about">About</Link>
+          </li>
+          <li>
+            <Link href="/projects">Projects</Link>
+          </li>
+          <li>
+            <Link href="/portfolio">Portfolio</Link>
+          </li>
+          <li>
+            <Link href="/contact">Contact</Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
